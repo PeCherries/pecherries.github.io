f83a25cb7644c4ac72bdf00c6b8605e37982dc02
Paul-PC\Paul <pecherries@gmail.com>
2019-07-20 10:13:15 -0400

    i just added everything

---
+++ pwcheck.js
@@ -0,0 +1,15 @@
+function validateForm() {
+        var pw = document.getElementById("inputPassword").value;
+        var password = "password";
+        //please don't judge me
+        if (pw == password) {
+            console.log("hey");
+            window.location.replace("https://photos.google.com/share/AF1QipMMSSGoJle1jGLYpNY1IkD4bERwuhGGG0e7V5u4JycgtCPUpgYof7piWipJvLIO7w?key=QUNRSG8wRVBpMEFxRWtJV0pJUkJZb0JHQXJIMUJn");
+            return false;
+        }
+        else {
+            alert ("Login was unsuccessful, please retry password");
+            console.log("well that worked");
+            return false;
+        }
+    }
